6d4c16496cff4d4187d88a83800019165a224588
CharlieA <audain27@gmail.com>
2025-05-07 17:04:37 +0100

    initial date based timer tech

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const timer = document.getElementById("timer");
 const timerText = timer.firstChild;
+let timerStarted = false;
 console.log(timerText.innerHTML);
 
-timer.addEventListener("click", () => setInterval(incrementTimer, 10));
+timer.addEventListener("click", startTimer());
 
-let timerValue = 0;
-
-function incrementTimer() {
-  timerValue += 1;
-
-  console.log(timerValue);
+function startTimer() {
+  let start = new Date();
+  let startTime = start.getTime();
+  timerStarted = true;
+  while (timerStarted == true) {
+    incrementTimer(start);
+  }
+}
+function incrementTimer(orig) {
+  now = new Date();
+  atm = now.getTime();
+  delta = atm - orig;
+  timerContent = delta / 100;
+  newFeed = timerContent.toFixed(2);
+  console.log;
+  timerText.textContent = newFeed;
 }
