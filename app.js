937c27831910b13ca2692fe72878628dbbb4496c
CharlieA <audain27@gmail.com>
2025-10-08 15:56:10 +0100

    Added remove button, added localstorage functionality

--- app.js
+++ app.js
@@ -2,21 +2,42 @@ const timer = document.getElementById("timer");
 const timerText = timer.firstChild;
 let startTime;
 let timerLoop = null;
-let timerState = "ready";
-
+let timerState;
+let heldTime = 0;
+let solves = [];
+const xbutton = timer.lastChild
 const doc = document.querySelector("body");
 function timerInit() {
+  loadSolves();
   timerText.innerText = "0.00";
   doc.addEventListener("keypress", (e) => {
     if (e.key === " ") {
       if (timerLoop) {
         timerStop();
       } else {
-        timerStart();
+        if (e.repeat == true) {
+          timerText.innerHTML = "0.00"
+          timerText.classList.add("red");
+          heldTime += 1;
+          console.log(heldTime);
+        }
+        if (heldTime > 10) {
+          timerText.classList.add("green");
+          timerText.classList.remove("red");
+          timerState = "ready";
+        }
       }
     }
   });
 }
+doc.addEventListener("keyup", (e) => {
+  if (e.key === " ") {
+    heldtime = 0;
+    timerText.classList.remove("green");
+    timerText.classList.remove("red");
+    timerStart();
+  }
+});
 
 function timerFunction(initial) {
   let elapsed = Date.now();
@@ -33,15 +54,34 @@ function timerStart() {
     timerLoop = setInterval(() => {
       timerFunction(startTime);
     });
+  } else {
+    heldTime = 0;
   }
 }
 
 function timerStop() {
   clearInterval(timerLoop);
   timerLoop = null;
-  setTimeout(() => {
-    timerState = "ready";
-  }, 2000);
+  solves.push(timerText.innerHTML);
+  saveSolves();
+  console.log(solves);
 }
 
+function saveSolves() {
+  let newSave = JSON.stringify(solves);
+  localStorage.setItem("solves", newSave);
+}
+
+function loadSolves() {
+  loadData = JSON.parse(localStorage.getItem("solves"));
+  solves = loadData;
+}
+
+function removeLast(){
+  solves.pop()
+  timerText.innerHTML = "0.00"
+
+}
+
+console.log(xbutton)
 timerInit();
