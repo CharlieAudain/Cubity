c2d450a7b6c566ffa20d02822c745d95ee6525eb
CharlieA <audain27@gmail.com>
2025-10-08 11:21:15 +0100

    Timer starts and stops on  spacebar press

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
 const timer = document.getElementById("timer");
 const timerText = timer.firstChild;
-let y = Date.now();
-const timerLoop = setInterval(timerFunction, 10);
+let startTime;
+let timerLoop = null;
+let timerState = "ready";
 
-function timerFunction() {
-  nowTime = Date.now()
-  let delta = nowTime - y;
-  
-  timerText.innerText = (delta/1000).toFixed(2);
+const doc = document.querySelector("body");
+function timerInit() {
+  timerText.innerText = "0.00";
+  doc.addEventListener("keypress", (e) => {
+    if (e.key === " ") {
+      if (timerLoop) {
+        timerStop();
+      } else {
+        timerStart();
+      }
+    }
+  });
 }
+
+function timerFunction(initial) {
+  let elapsed = Date.now();
+  let delta = elapsed - initial;
+
+  timerText.innerText = (delta / 1000).toFixed(2);
+  console.log(delta);
+}
+
+function timerStart() {
+  startTime = Date.now();
+  if (timerState == "ready") {
+    timerState = "engaged";
+    timerLoop = setInterval(() => {
+      timerFunction(startTime);
+    });
+  }
+}
+
+function timerStop() {
+  clearInterval(timerLoop);
+  timerLoop = null;
+  setTimeout(() => {
+    timerState = "ready";
+  }, 2000);
+}
+
+timerInit();
