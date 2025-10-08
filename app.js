c7f7355112f9d69fa3705efd4c0bf02ed6720a52
CharlieA <audain27@gmail.com>
2025-10-08 08:45:52 +0100

    Create Timer Logic

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
 const timer = document.getElementById("timer");
 const timerText = timer.firstChild;
-let timerStarted = false;
-console.log(timerText.innerHTML);
+let y = Date.now();
+const timerLoop = setInterval(timerFunction, 10);
 
-timer.addEventListener("click", startTimer());
-
-function startTimer() {
-  let start = new Date();
-  let startTime = start.getTime();
-  timerStarted = true;
-  while (timerStarted == true) {
-    incrementTimer(start);
-  }
-}
-function incrementTimer(orig) {
-  now = new Date();
-  atm = now.getTime();
-  delta = atm - orig;
-  timerContent = delta / 100;
-  newFeed = timerContent.toFixed(2);
-  console.log;
-  timerText.textContent = newFeed;
+function timerFunction() {
+  nowTime = Date.now()
+  let delta = nowTime - y;
+  
+  timerText.innerText = (delta/1000).toFixed(2);
 }
